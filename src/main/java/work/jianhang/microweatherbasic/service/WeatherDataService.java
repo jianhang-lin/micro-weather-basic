015e92c1be07180929d19bd6e1b2080339d5667e
jianhang-lin <ej2741@email.vccs.edu>
2018-06-25 11:37:52 +0800

    更新天气数据服务接口返回的数据类型

--- src/main/java/work/jianhang/microweatherbasic/service/WeatherDataService.java
+++ src/main/java/work/jianhang/microweatherbasic/service/WeatherDataService.java
@@ -1,5 +1,7 @@
 package work.jianhang.microweatherbasic.service;
 
+import work.jianhang.microweatherbasic.vo.WeatherResponse;
+
 /**
  * 天气数据服务接口
  *
@@ -11,15 +13,15 @@ public interface WeatherDataService {
      * 根据城市ID查询天气数据
      *
      * @param cityId 城市ID
-     * @return WeatherDataService
+     * @return WeatherResponse
      */
-    WeatherDataService getDataByCityId(String cityId);
+    WeatherResponse getDataByCityId(String cityId);
 
     /**
      * 根据城市名称查询天气数据
      *
      * @param cityName 城市名称
-     * @return WeatherDataService
+     * @return WeatherResponse
      */
-    WeatherDataService getDataByCityName(String cityName);
+    WeatherResponse getDataByCityName(String cityName);
 }
