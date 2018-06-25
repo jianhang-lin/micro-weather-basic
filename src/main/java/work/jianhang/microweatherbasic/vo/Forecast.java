46a77fc40be9c465c9cd41c93115da2319e80dcb
jianhang-lin <ej2741@email.vccs.edu>
2018-06-25 11:12:43 +0800

    未来天气信息类

---
+++ src/main/java/work/jianhang/microweatherbasic/vo/Forecast.java
@@ -0,0 +1,76 @@
+package work.jianhang.microweatherbasic.vo;
+
+import java.io.Serializable;
+
+/**
+ * 未来天气信息
+ *
+ * @author jianhanglin
+ */
+public class Forecast implements Serializable {
+
+    private static final long serialVersionUID = 8622438725029703936L;
+
+    private String date;
+
+    private String hight;
+
+    private String fengli;
+
+    private String low;
+
+    private String fengxiang;
+
+    private String type;
+
+    public Forecast() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHight() {
+        return hight;
+    }
+
+    public void setHight(String hight) {
+        this.hight = hight;
+    }
+
+    public String getFengli() {
+        return fengli;
+    }
+
+    public void setFengli(String fengli) {
+        this.fengli = fengli;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getFengxiang() {
+        return fengxiang;
+    }
+
+    public void setFengxiang(String fengxiang) {
+        this.fengxiang = fengxiang;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
